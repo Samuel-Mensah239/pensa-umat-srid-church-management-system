0b5706a8f4e52b0bf1abc653bf119af64bf93c2d
Samuel-Mensah239 <sm2370120@gmail.com>
2026-03-28 01:51:53 -0700

    Setup Docker and ignore local database

--- split pensa umat srid/src/main/java/com/pensa/umat/srid/PensaUmatSridApplication.java
+++ split pensa umat srid/src/main/java/com/pensa/umat/srid/PensaUmatSridApplication.java
@@ -2,8 +2,9 @@ package com.pensa.umat.srid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration; // Add this import
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class }) // Add the exclude here
 public class PensaUmatSridApplication {
   public static void main(String[] args) {
     SpringApplication.run(PensaUmatSridApplication.class, args);
